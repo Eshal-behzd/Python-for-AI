d6c4506adce012332097e97ecea6a0c393c76188
Eshal-behzd <behzadeshal5@gmail.com>
2026-08-13 04:00:34 -0700

    Add initial implementation of data types in Python

---
+++ Datatypes.py
@@ -0,0 +1,22 @@
+# Integer : Whole numbers, both positive and negative, without decimal points. Examples: -3, 0, 42
+age  = 24
+print (age)
+
+# float : Numbers that contain decimal points. Examples: 3.14, -0.001, 2.0
+pi = 3.14
+print (pi)
+
+# String : A sequence of characters enclosed in single or double quotes. Examples: 'Hello', "World", '123'
+name = "John Doe"
+print (name)    
+
+# Boolean : Represents one of two values: True or False. Examples: True, False
+is_student = True     
+print (is_student)
+
+# Catenation : The process of joining two or more strings together. In Python, you can use the + operator to concatenate strings. Example: "Hello" + " " + "World" results in "Hello World"
+greeting = "Hello"
+message = "World"
+full_message = greeting + " " + message
+print (full_message)
+
