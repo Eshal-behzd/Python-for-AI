2b1639ec7bbd3903ab333ad1fb1206a666349376
Eshal-behzd <behzadeshal5@gmail.com>
2026-08-14 09:51:05 -0700

    Add initial implementation of Dictionary data structure with basic methods and operations

---
+++ Dictionary.py
@@ -0,0 +1,29 @@
+# Dictionary: unordered collection of key-value pairs
+Student = {
+    "name": "John",
+    "age": 20,
+    "course": "Computer Science"
+}
+print("Student:", Student)
+
+# Accessing values in a dictionary using keys
+print(Student["name"])
+print(Student.get("age"))
+
+# Methods
+
+# Adding a new key-value pair to the dictionary
+Student["city"] = "New York"
+print(Student)
+
+# updating an existing key-value pair in the dictionary
+Student["course"] = "Data Science"
+print(Student)
+
+# Removing a key-value pair from the dictionary using the del statement
+Student.pop("course")
+print(Student)
+
+# loops through dictionary
+for key,value in Student.items():
+    print(key, ":", value)
