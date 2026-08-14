5f3e06175a790f4eafaca0b240ac0d6ca3c36106
Eshal-behzd <behzadeshal5@gmail.com>
2026-08-14 10:00:31 -0700

    Add initial implementation of Practice.py with examples of list, tuple, set, and dictionary

---
+++ Practice.py
@@ -0,0 +1,22 @@
+# Create a list of 5 numbers and one more number.
+numbers= [1, 2, 3, 4, 5]
+numbers.append(6)
+print(numbers)
+
+# Create a tuple of 3 cities and print the second city.
+cities = ("New York", "Los Angeles", "Chicago")
+print(cities[1])
+
+# Create 2 sets and find intersection of the two sets.
+A= {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+print(A.intersection(B))
+
+# Create a dictionary of your own and print all keys 
+data = {
+    "Name" : "Eshal Behzad",
+    "Age" : 19,
+    "City" : "Mardan",
+    "Education" : "Bachelor's"
+}
+print(data)
