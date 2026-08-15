2a26e3ee530aadd0334ef29a09dbcfc23fd10b16
Eshal-behzd <behzadeshal5@gmail.com>
2026-08-15 03:57:44 -0700

    Add initial implementation of Type Casting example in Type_Casting.py

---
+++ Type_Casting.py
@@ -0,0 +1,13 @@
+# Type Casting: The process of converting one data type to another
+age = "12"     
+print(age)
+print(type(age))
+
+# Converting string to integer
+print(int(age))
+print(int(age) + 5)
+
+my_age = input("Please enter your age:")
+print("my_age:", my_age)
+print(type(my_age))
+
