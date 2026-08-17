5c020b46c125076d056bd1c79c1d9c63a0d011d1
Eshal-behzd <behzadeshal5@gmail.com>
2026-08-17 11:02:30 -0700

    Added looping over Strings

---
+++ Looping_over_Strings.py
@@ -0,0 +1,15 @@
+for char in "PYTHON":
+    print(char)
+
+count = 0
+for char in "banana":
+    if char == "a":
+        count += 1
+print(count)         
+
+s = "PYTHON"
+rev = "" 
+for ch in s:
+    rev = ch + rev
+print(rev) 
+
