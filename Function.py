4de6b86c274e2c0792b0c453053a7e542a5069a6
Eshal-behzd <behzadeshal5@gmail.com>
2026-08-18 20:04:34 -0700

    Add initial implementation of Function.py with greet function examples

---
+++ Function.py
@@ -0,0 +1,11 @@
+def greet():
+    print("Hello World!")
+# call the function 
+greet()
+
+# function with parameter
+def greet(name):
+    print("Hello, " + name + "!")
+greet("John")
+
+greet("Alice")
