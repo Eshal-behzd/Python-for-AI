301f0352ce9d9ad3993ed6312d4bd5ad387d4fd0
Eshal-behzd <behzadeshal5@gmail.com>
2026-08-20 11:56:34 -0700

    Fix typos in eligibility messages and improve code comments in Student class

--- Student.py
+++ Student.py
@@ -1,6 +1,7 @@
 class Student:
+    # constructor method
     def __init__(self, name, age, grade):
-        self.name = name 
+        self.name = name    #instance variable
         self.age = age
         self.grade = grade
     def display_info(self):
@@ -11,15 +12,18 @@ class Student:
     
     def is_eligible(self):
         if self.age >= 15 :
-            print(f"{self.name} is eligilbe for Admission.")  
+            print(f"{self.name} is eligible for Admission.")  
         else:
-            print(f"{self.name} is not eligilbe for Admission.")    
-
+            print(f"{self.name} is not eligible for Admission.")    
+# creating objects
 student1 = Student("Bob" , 23 , "A")
-
+# accessing attributes
 print(student1.name)
 print(student1.age)
-print(student1.grade)
+
+# calling method
+
+student1.display_info()
 student1.is_eligible()
 
 
