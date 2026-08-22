62acb7ad85cb291e7d6a10c21f2db44846312377
Eshal-behzd <behzadeshal5@gmail.com>
2026-08-22 04:33:35 -0700

    Enhance Polymorphism.py with additional comments explaining method overriding and default arguments

--- Polymorphism.py
+++ Polymorphism.py
@@ -1,3 +1,4 @@
+# Polymorphism: Different classes have the same method name
 class Dog:
     def speak(self):
         print("Woof!")
@@ -9,7 +10,7 @@ class Cat:
 for pet in [Dog(), Cat()]:
     pet.speak()
 
-
+# Method Overriding: Child class redefines the parent's method
 class Vehicle:
     def move(self):
         print("Vehicle is moving!")
@@ -20,10 +21,14 @@ class Car(Vehicle):
 car = Car()
 car.move()
 
+# Default Arguments: A method can work with different numbers of argument
 class Calculator:
     def add (self, a, b=0, c=0):
         return a + b + c
 cal = Calculator()
+# Only a is provided; b and c use their default value 0
 print(cal.add(5))
+# a and b are provided; c uses its default value 0
 print(cal.add(3, 6))
+# All three arguments are provided
 print(cal.add(2, 5, 1)) 
