a9ad2fc39ea0662d2eda8ee50e2b455500ecce7a
Eshal-behzd <behzadeshal5@gmail.com>
2026-08-25 08:37:57 -0700

    Add Practice1.py for array manipulation and conversion to float

---
+++ Practice1.py
@@ -0,0 +1,6 @@
+import numpy as np
+marks = np.array([78, 85, 92, 67, 88])
+print(f"original array:{marks}")
+float_marks = marks.astype(float)
+print(f"After converted to float :{float_marks}")
+  
